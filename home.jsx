7d45263286fe376c3492d0b7c119cacdbb576e17
Chitra <126138761+Chitra-P-99@users.noreply.github.com>
2023-07-18 17:09:34 +0530

    Add files via upload

---
+++ home.jsx
@@ -0,0 +1,15 @@
+import Carousel from "./carousel";
+import Favourites from "./favourites";
+
+import Movies from "./movies";
+let Home=()=>{
+    return(
+        <div className="home">
+            <Carousel/>
+            <Movies/>
+          
+           
+        </div>
+    )
+}
+export default Home;
